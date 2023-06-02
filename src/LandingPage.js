f4a1a51ac50c377a83e57f6d1341cac9c4ba74c5
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2023-06-02 09:37:54 +0100

    connect

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -77,7 +77,7 @@ const LandingPage = () => {
                             <path d="M25,2C12.297,2,2,12.297,2,25s10.297,23,23,23s23-10.297,23-23S37.703,2,25,2z M25,11c1.657,0,3,1.343,3,3s-1.343,3-3,3 s-3-1.343-3-3S23.343,11,25,11z M29,38h-2h-4h-2v-2h2V23h-2v-2h2h4v2v13h2V38z"></path>
                         </svg>
                     </a>
-                    {hasProvider && <button onClick={handleConnect}>Click to connect Metamask</button>}
+                     <button onClick={handleConnect}>Click to connect Metamask</button>
                 </div>
                 <div>Injected Provider {hasProvider ? 'DOES' : 'DOES NOT'} Exist</div>
                 <p>
